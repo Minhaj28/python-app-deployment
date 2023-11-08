263808cd2744adbe469e1a429069cf20b5a7d389
Minhaj28 <mdminhajuddin.official@gmail.com>
2023-11-08 18:23:26 +0000

    execute only run.sh and deploy the app

--- python-fast-app/shells/run.sh
+++ python-fast-app/shells/run.sh
@@ -1,3 +1,13 @@
-cd /home/ubuntu_server/appdeployment/python-app-deployment/python-fast-app
+sudo apt update
+sudo apt install -y python3-pip python3-venv
+
+# Create and activate the virtual environment
+python3 -m venv venv
 source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+sudo apt-get -y install uvicorn
+
+cd /home/ubuntu_server/appdeployment/python-app-deployment/python-fast-app
 nohup uvicorn main:app --host 0.0.0.0 --port 8000 > app.log 2>&1 &
