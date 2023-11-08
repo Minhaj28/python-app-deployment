f2cbe5336e059ddbc3d8ef54eb805979a6562150
Minhaj28 <mdminhajuddin.official@gmail.com>
2023-11-08 17:59:56 +0000

    deploy fast app

--- python-fast-app/database.py
+++ python-fast-app/database.py
@@ -7,9 +7,9 @@ time.sleep(5)  # Adjust the delay as needed
 db_config = {
     "host": "localhost",
     "port": 3306,
-    "user": "root",
-    "password": "root",
-    "database": "moviedb",
+    "user": "admin",
+    "password": "admin",
+    "database": "movieDB",
 }
 
 # Establish a database connection
--- python-fast-app/shells/run.sh
+++ python-fast-app/shells/run.sh
@@ -1,2 +1,3 @@
-cd /home/ubuntu
-uvicorn main:app --host 0.0.0.0 --port 8000 > /dev/app.log 2>&1 &
+cd /home/ubuntu_server/appdeployment/python-app-deployment/python-fast-app
+source venv/bin/activate
+nohup uvicorn main:app --host 0.0.0.0 --port 8000 > app.log 2>&1 &
